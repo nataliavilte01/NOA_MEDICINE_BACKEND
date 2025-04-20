c203131dedd25584a54f61b838e336bc5ff71eab
nataliavilte01 <natyvilte03@gmail.com>
2025-04-19 21:08:05 -0300

    Cambios en la configuracion inicial para trabajar con variables de entorno

--- bin/database.js
+++ bin/database.js
@@ -1,10 +1,8 @@
+require('dotenv').config('../.env');
 const mongoose = require ('mongoose');
-const URI= process.env.URI_DB;
-
+const URI= process.env.URI_DB
 mongoose.connect(URI)
         .then (db=> console.log('DB is connected'))
         .catch( error => console.error(error));
 
 module.exports = mongoose;
-
-
--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,7 @@
+require ('dotenv').config('../.env');
 const app= require ('../app.js')
 const {mongoose} = require ('./database.js');
+console.log(process.env.PORT)
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), ()=> {
